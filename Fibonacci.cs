06c0eb9ac24798572cb6fe64eec5170b40f48efe
Henrique <miniqui10@gmail.com>
2020-06-25 16:19:53 -0300

    Recursividade

---
+++ Fibonacci.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Recursividade
+{
+    public class Fibonacci
+    {
+        public void GerarSequenciaFibonacci(int numero1, int numero2, int vezes)
+
+        {
+            if(vezes >= 1){
+                System.Console.WriteLine(numero1);
+                GerarSequenciaFibonacci(numero2, numero1 + numero2, vezes -1);
+            }
+        }
+
+
+        public int GerarFatorial(int numero){
+            if(numero == 1){
+                return 1;
+            }else{
+                return numero * GerarFatorial(numero -1);
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            System.Console.WriteLine("Sequência Fibonacci");
+            fibo.GerarSequenciaFibonacci(0,1,13);
+            System.Console.WriteLine("Sequência fatorial");
+            System.Console.WriteLine(fibo.GerarFatorial(8));
+        }
+    }
+}
